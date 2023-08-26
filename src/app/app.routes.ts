dc3d49ce891875a87d6fce4b7d3714003995a645
Rodion Sibov <rodionsibov@gmail.com>
2023-08-26 21:49:57 +0300

    Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'tutorials',
+    loadComponent: () =>
+      import('./components/tutorials-list/tutorials-list.component').then(
+        (x) => x.TutorialsListComponent
+      ),
+  },
+  {
+    path: 'tutorials:id',
+    loadComponent: () =>
+      import('./components/tutorial-details/tutorial-details.component').then(
+        (x) => x.TutorialDetailsComponent
+      ),
+  },
+  {
+    path: 'add',
+    loadComponent: () =>
+      import('./components/add-tutorial/add-tutorial.component').then(
+        (x) => x.AddTutorialComponent
+      ),
+  },
+];
